165a9b108b2cbb4d1ff7c299f49f3cf1d92dae2c
lucas fasilleau <lucasfaz@hotmail.fr>
2022-12-19 15:18:18 +0100

    ajout page login et page createUser

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
-      <Header />
       <h2> Bienvenue </h2>
       <div>
         <Link className="simpleLink" to="/tutorialTheme">
