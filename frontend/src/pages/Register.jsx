2029b18d2af7c9d59b30edf1421158835a8f2b29
lucas fasilleau <lucasfaz@hotmail.fr>
2023-01-03 12:21:44 +0100

    fix bug on register page and translate

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -40,18 +40,18 @@ function Register() {
     firstName === "" ||
     lastname === "" ? (
       <button
-        type="button"
+        type="submit"
         disabled
         className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
       >
-        Submit
+        Envoyez
       </button>
     ) : (
       <button
-        type="button"
+        type="submit"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        Submit
+        Envoyez
       </button>
     );
 
@@ -73,7 +73,7 @@ function Register() {
             htmlFor="floating_email"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Email address
+            Adresse e-mail
           </label>
         </div>
         <div className="relative z-0 mb-6 w-full group">
@@ -91,7 +91,7 @@ function Register() {
             htmlFor="floating_password"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Password
+            Mot de passe
           </label>
         </div>
         <div className="relative z-0 mb-6 w-full group">
@@ -128,7 +128,7 @@ function Register() {
               htmlFor="floating_first_name"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              First name
+              Prénom
             </label>
           </div>
           <div className="relative z-0 mb-6 w-full group">
@@ -146,7 +146,7 @@ function Register() {
               htmlFor="floating_last_name"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Last name
+              Nom
             </label>
           </div>
         </div>
