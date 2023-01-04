63b11406e3eb062d87a350929338380fe5f73fe8
Romain Timmer <timmer.romain@free.fr>
2023-01-04 11:19:47 +0100

    changes in database

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -40,7 +40,7 @@ function Register() {
     firstName === "" ||
     lastname === "" ? (
       <button
-        type="button"
+        type="submit"
         disabled
         className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
       >
@@ -48,7 +48,7 @@ function Register() {
       </button>
     ) : (
       <button
-        type="button"
+        type="submit"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Submit
