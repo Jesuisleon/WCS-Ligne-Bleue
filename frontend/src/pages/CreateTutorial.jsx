5572f63cb509465ac3c1c462ef8cb6ca25aad8b1
Jesuisleon <frederic-versavel_student2022@wilder.school>
2023-01-06 10:39:44 +0100

    spt2_front-dev Possibily to create and edit a quiz

--- frontend/src/pages/CreateTutorial.jsx
+++ frontend/src/pages/CreateTutorial.jsx
@@ -1,69 +1,230 @@
 import React, { useState, useEffect } from "react";
 import { Editor } from "@tinymce/tinymce-react";
-import Quiz from "@components/Quiz";
 import axios from "axios";
 import stockedData from "./data";
 
-const quizData = [
+const { VITE_BACKEND_URL } = import.meta.env;
+
+const quizDataForTest = [
   {
-    id: 1,
-    question: "Quelle est la capitale de la France?",
-    answers: [
-      { id: 1, text: "Londres", correct: false },
-      { id: 2, text: "Paris", correct: true },
-      { id: 3, text: "Berlin", correct: false },
-      { id: 4, text: "New York", correct: false },
-    ],
-  },
-  {
-    id: 2,
-    question: "Combien y a-t-il de jours dans une année?",
-    answers: [
-      { id: 1, text: "365", correct: true },
-      { id: 2, text: "366", correct: false },
-      { id: 3, text: "364", correct: false },
-      { id: 4, text: "360", correct: false },
+    type: "quiz",
+    step: "",
+    data: [
+      {
+        id: 1,
+        question: "Quelle est la capitale de la France?",
+        answers: [
+          { id: 1, text: "Londres", correct: false },
+          { id: 2, text: "Paris", correct: true },
+          { id: 3, text: "Berlin", correct: false },
+          { id: 4, text: "New York", correct: false },
+        ],
+      },
+      {
+        id: 2,
+        question: "Combien y a-t-il de jours dans une année?",
+        answers: [
+          { id: 1, text: "365", correct: true },
+          { id: 2, text: "366", correct: false },
+          { id: 3, text: "364", correct: false },
+          { id: 4, text: "360", correct: false },
+        ],
+      },
     ],
   },
 ];
 
-function EditorForTopData({
-  handleInputChange,
-  id,
-  placeholder,
-  defaultValue,
-}) {
+function CreateQuizz({ submitData }) {
+  const step = "2";
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (quizDataForTest.length > 0) setData(quizDataForTest[0].data);
+  }, []);
+
+  function addNewQuestion() {
+    const newQuestion = { id: data.length + 1, question: "", answers: [] };
+    const newQuestionArray = [...data, newQuestion];
+    setData(newQuestionArray);
+  }
+
+  function handleQuestionChange(e) {
+    const { value, id } = e.target;
+    const newQuestionArray = [...data];
+    newQuestionArray[id].question = value;
+    setData(newQuestionArray);
+  }
+
+  function addNewAnswer(questionId) {
+    const newAnswer = {
+      id: data[questionId].answers.length + 1,
+      text: "",
+      correct: false,
+    };
+
+    const newAnswerArray = data.map((question) => {
+      if (question.id === questionId + 1) {
+        return { ...question, answers: [...question.answers, newAnswer] };
+      }
+      return question;
+    });
+    setData(newAnswerArray);
+  }
+
+  function handleAnswerChange(e) {
+    const { value, id, name } = e.target;
+    const newAnswerArray = [...data];
+    newAnswerArray[id].answers[name].text = value;
+    setData(newAnswerArray);
+  }
+
+  function handleAnswerCorrect(e) {
+    const { id, name } = e.target;
+    const newAnswerArray = [...data];
+    newAnswerArray[id].answers[name].correct =
+      !newAnswerArray[id].answers[name].correct;
+    setData(newAnswerArray);
+  }
+
+  function deleteQuestion(id) {
+    const newQuestionArray = data.filter((e) => e.id !== id);
+    setData(newQuestionArray);
+  }
+
+  function deleteAnswer(questionId, answerId) {
+    const newAnswerArray = data.map((question) => {
+      if (question.id === questionId) {
+        return {
+          ...question,
+          answers: question.answers.filter((answer) => answer.id !== answerId),
+        };
+      }
+      return question;
+    });
+    setData(newAnswerArray);
+  }
+
+  function handleSubmitData(e) {
+    e.preventDefault();
+    submitData({ step, quiz: data });
+  }
+
   return (
-    <Editor
-      apiKey="9t40bxm0c2coeb5xw6ui3a8g1ng5eywiv3elxxlq6vtclsdy"
-      id={id}
-      onEditorChange={handleInputChange}
-      value={defaultValue}
-      init={{
-        selector: "#theEditorForTopData",
-        fixed_toolbar_container: "#toolbar-container-top",
-        placeholder,
-        statusbar: false,
-        menubar: false,
-        inline: true,
-        content_style: "body { font-family: Montserrat, Arial, sans-serif }",
-        toolbar: "bold italic underline ",
-      }}
-    />
+    <div className="p-4 h-full w-full bg-white rounded-r-lg mb-4">
+      <form
+        action="submit"
+        onSubmit={handleSubmitData}
+        className="flex flex-col gap-4 items-start justify-start w-full"
+      >
+        {data.map((question) => (
+          <div key={question.id} className="flex flex-col items-start gap-4">
+            <div className="border-b-2 p-1 flex gap-4 items-center w-full">
+              <button
+                type="button"
+                className="bg-red-400 text-sm text-white rounded px-1"
+                onClick={() => {
+                  if (
+                    window.confirm(
+                      "Êtes-vous sûr de vouloir supprimer cette question ?"
+                    )
+                  )
+                    deleteQuestion(question.id);
+                }}
+              >
+                X
+              </button>
+              <label htmlFor="question" className="text-xl font-bold">
+                {question.id}.
+                <input
+                  placeholder="Votre Question"
+                  size={question.question.length}
+                  className="text-xl font-bold"
+                  type="text"
+                  id={question.id - 1}
+                  value={question.question}
+                  onChange={(e) => {
+                    handleQuestionChange(e);
+                  }}
+                />
+              </label>
+            </div>
+            {question.answers.map((answer) => (
+              <div key={answer.id} className="flex gap-4 items-center w-full">
+                <button
+                  type="button"
+                  className="bg-red-400 text-sm text-white rounded px-1"
+                  onClick={() => {
+                    if (
+                      window.confirm(
+                        "Êtes-vous sûr de vouloir supprimer cette réponse ?"
+                      )
+                    )
+                      deleteAnswer(question.id, answer.id);
+                  }}
+                >
+                  X
+                </button>
+                <input
+                  type="checkbox"
+                  id={question.id - 1}
+                  name={answer.id - 1}
+                  checked={answer.correct}
+                  onChange={(e) => {
+                    handleAnswerCorrect(e);
+                  }}
+                />
+                <input
+                  placeholder="Votre réponse"
+                  className={`${answer.correct && "text-green-500"}`}
+                  size={answer.text.length}
+                  type="text"
+                  name={answer.id - 1}
+                  id={question.id - 1}
+                  value={answer.text}
+                  onChange={(e) => {
+                    handleAnswerChange(e);
+                  }}
+                />
+              </div>
+            ))}
+            <button
+              className="bg-green-500 text-white font-bold py-2 px-4 rounded"
+              type="button"
+              onClick={() => addNewAnswer(question.id - 1)}
+            >
+              Ajouter une réponse
+            </button>
+          </div>
+        ))}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="button"
+          onClick={addNewQuestion}
+        >
+          Nouvelle question
+        </button>
+
+        <button
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Valider
+        </button>
+      </form>
+    </div>
   );
 }
 
 function CreateTutorial() {
-  const { VITE_BACKEND_URL } = import.meta.env;
-
   const author = "Michel";
-  const themeId = "2";
 
   const initialData = [].concat(stockedData);
 
   const [editorData, setEditorData] = useState([]);
 
   const [anotherData, setAnotherData] = useState([]);
+
+  const [quizData, setQuizData] = useState([]);
 
   useEffect(() => {
     setEditorData(initialData.filter(({ step }) => step));
@@ -77,14 +238,51 @@ function CreateTutorial() {
       objective: anotherData.objective,
       description: anotherData.description,
       difficulty: anotherData.difficulty,
-      content: JSON.stringify(editorData),
+      content: JSON.stringify(editorData) + JSON.stringify(quizData),
       hashtag: anotherData.hashtag,
       author,
-      theme: themeId,
+      theme: anotherData.theme,
     };
-    // stockedData.splice(0, stockedData.length, ...editorData, anotherData);
+    // console.log(JSON.stringify(data));
     axios.post(`${VITE_BACKEND_URL}/tutorials`, data);
   }
+  // to store the image date from the editor to the backend
+  const handleImageEditor = (blobInfo, progress) => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.withCredentials = false;
+      xhr.open("POST", `${VITE_BACKEND_URL}/upload/image`);
+      xhr.upload.onprogress = (e) => {
+        progress((e.loaded / e.total) * 100);
+      };
+      xhr.onload = () => {
+        if (xhr.status === 403) {
+          reject(new Error());
+          return;
+        }
+        if (xhr.status < 200 || xhr.status >= 300) {
+          reject(new Error(`HTTP Error: ${xhr.status}`));
+          return;
+        }
+        const json = JSON.parse(xhr.responseText);
+        if (!json || typeof json.location !== "string") {
+          reject(new Error(`Invalid JSON: ${xhr.responseText}`));
+          return;
+        }
+        resolve(json.location);
+      };
+      xhr.onerror = () => {
+        reject(
+          new Error(
+            `Image upload failed due to a XHR Transport error. Code: ${xhr.status}`
+          )
+        );
+      };
+      const formData = new FormData();
+      formData.append("image", blobInfo.blob(), blobInfo.filename());
+      xhr.send(formData);
+    });
+  };
 
   function removeBlockAndUpdateStep(blockToRemove) {
     const newStepArray = editorData.filter(
@@ -99,7 +297,7 @@ function CreateTutorial() {
   }
 
   function addNewBlockAndUpdateStep() {
-    const newStep = { step: editorData.length + 1, content: "" };
+    const newStep = { step: editorData.length + 1, text: "", type: "text" };
     const newStepArray = [...editorData, newStep];
     setEditorData(newStepArray);
   }
@@ -132,68 +330,108 @@ function CreateTutorial() {
         >
           <p>Enregistrer</p>
         </button>
-        <div className="flex items-center gap-4 mb-4">
-          <div className="flex flex-col items-center gap-4">
-            <h2 className="text-2xl font-bold">Hashtag</h2>
-            <input
+        <div className="flex items-center gap-4 mb-4 bg-white w-full justify-around p-6">
+          <div className="flex flex-col gap-2 items-start ">
+            <h2 className="text-2xl font-bold">Théme</h2>
+            <select
               onChange={(e) =>
-                handleInputChange(e.target.value, { id: "hashtag" })
+                handleInputChange(e.target.value, { id: "theme" })
               }
-              placeholder="Vos hashtag"
-              id="hashtag"
-              type="text"
-            />
+              name="theme"
+              id="theme"
+            >
+              <option value="1">Utiliser ligne Bleue</option>
+              <option value="2">Utiliser mon téléphone</option>
+              <option value="3">Aller sur internet</option>
+            </select>
           </div>
-          <div className="flex flex-col gap-2 items-center ">
+          <div className="flex flex-col gap-2 items-start ">
             <h2 className="text-2xl font-bold">Niveau</h2>
             <select
               onChange={(e) =>
                 handleInputChange(e.target.value, { id: "difficulty" })
               }
-              name="level"
-              id="level"
+              name="difficulty"
+              id="difficulty"
             >
               <option value="Débutant">Débutant</option>
               <option value="Facile">Facile</option>
               <option value="Novice">Novice</option>
             </select>
           </div>
+          <div className="flex flex-col items-start gap-2">
+            <h2 className="text-2xl font-bold">Hashtag</h2>
+            <input
+              onChange={(e) =>
+                handleInputChange(e.target.value, { id: "hashtag" })
+              }
+              placeholder="Vos hashtag"
+              name="hashtag"
+              id="hashtag"
+              type="textarea"
+              defaultValue={anotherData ? anotherData.hashtag : ""}
+            />
+          </div>
         </div>
-        <div
-          id="toolbar-container-top"
-          className="h-10 w-full bg-white border-b-2 drop-shadow-sm rounded-t-lg"
-        />
-        <div className="bg-white p-6 w-full grid justify-center items-center gap-4 grid-cols-4 lg:grid-cols-6 auto-rows-auto lg:grid-rows-1 border-b-2">
+        <div className="bg-white p-6 w-full grid justify-center items-center gap-4 grid-cols-4 lg:grid-cols-6 auto-rows-auto lg:grid-rows-1 border-y-2">
           <div className="col-span-1 m-auto">
             <img src="https://via.placeholder.com/150" alt="logo" />
           </div>
           <div className="col-span-3 lg:col-span-2 flex flex-col gap-4">
-            <h2 className="text-2xl font-bold">Titre</h2>
-            <EditorForTopData
-              handleInputChange={(e) => handleInputChange(e, { id: "title" })}
-              id="title"
-              placeholder="Choisissez un titre"
-              defaultValue={anotherData ? anotherData.title : ""}
-            />
-            <h2 className="text-2xl font-bold">Objectif</h2>
-            <EditorForTopData
-              handleInputChange={(e) =>
-                handleInputChange(e, { id: "objective" })
-              }
-              id="objective"
-              placeholder="Décrivez l'objectif de ce tutoriel"
-              defaultValue={anotherData ? anotherData.objective : ""}
-            />
+            <div className="flex flex-col">
+              <label htmlFor="title" className="text-2xl font-bold">
+                Titre
+              </label>
+              <input
+                onChange={(e) =>
+                  handleInputChange(e.target.value, { id: "title" })
+                }
+                placeholder="Votre titre"
+                name="title"
+                id="title"
+                type="text"
+                defaultValue={anotherData ? anotherData.title : ""}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="objective" className="text-2xl font-bold">
+                Objectif
+              </label>
+              <input
+                onChange={(e) =>
+                  handleInputChange(e.target.value, { id: "objective" })
+                }
+                placeholder="Votre objectif"
+                name="objective"
+                id="objective"
+                type="text"
+                defaultValue={anotherData ? anotherData.objective : ""}
+              />
+            </div>
           </div>
-          <div className=" flex flex-col gap-4 col-span-4 lg:col-span-3 lg:row-auto h-full">
+          <div className=" flex flex-col gap-2 col-span-4 lg:col-span-3 lg:row-auto h-full">
             <h2 className="text-2xl font-bold">Description</h2>
-            <EditorForTopData
-              handleInputChange={(e) =>
+            <Editor
+              apiKey="9t40bxm0c2coeb5xw6ui3a8g1ng5eywiv3elxxlq6vtclsdy"
+              id="description"
+              onEditorChange={(e) =>
                 handleInputChange(e, { id: "description" })
               }
-              id="description"
-              placeholder="Votre description"
-              defaultValue={anotherData ? anotherData.description : ""}
+              value={anotherData ? anotherData.description : ""}
+              init={{
+                selector: "#theEditorForTopData",
+                autoresize_bottom_margin: 0,
+                statusbar: false,
+                placeholder: "Votre description",
+                toolbar_sticky: true,
+                menubar: false,
+                editimage_cors_hosts: ["picsum.photos"],
+                content_style:
+                  "body { font-family: Montserrat, Arial, sans-serif }",
+                plugins: "autoresize image",
+                toolbar: "bold italic underline image ",
+                images_upload_handler: handleImageEditor,
+              }}
             />
           </div>
         </div>
@@ -238,63 +476,21 @@ function CreateTutorial() {
                       },
                     });
                   },
-                  images_upload_handler: (blobInfo, progress) => {
-                    return new Promise((resolve, reject) => {
-                      const xhr = new XMLHttpRequest();
-                      xhr.withCredentials = false;
-                      xhr.open("POST", `${VITE_BACKEND_URL}/upload/image`);
-                      xhr.upload.onprogress = (e) => {
-                        progress((e.loaded / e.total) * 100);
-                      };
-                      xhr.onload = () => {
-                        if (xhr.status === 403) {
-                          reject(new Error());
-                          return;
-                        }
-                        if (xhr.status < 200 || xhr.status >= 300) {
-                          reject(new Error(`HTTP Error: ${xhr.status}`));
-                          return;
-                        }
-                        const json = JSON.parse(xhr.responseText);
-                        if (!json || typeof json.location !== "string") {
-                          reject(
-                            new Error(`Invalid JSON: ${xhr.responseText}`)
-                          );
-                          return;
-                        }
-                        resolve(json.location);
-                      };
-                      xhr.onerror = () => {
-                        reject(
-                          new Error(
-                            `Image upload failed due to a XHR Transport error. Code: ${xhr.status}`
-                          )
-                        );
-                      };
-                      const formData = new FormData();
-                      formData.append(
-                        "image",
-                        blobInfo.blob(),
-                        blobInfo.filename()
-                      );
-                      xhr.send(formData);
-                    });
-                  },
+                  images_upload_handler: handleImageEditor,
                 }}
               />
             </div>
           );
         })}
-
-        <button
-          type="button"
-          className="m-4 p-2 rounded-xl bg-white cursor-pointer text-lg shadow-md hover:bg-black hover:text-white"
-          onClick={() => addNewBlockAndUpdateStep()}
-        >
-          <p>Nouvelle section</p>
-        </button>
       </form>
-      <Quiz data={quizData} />
+      <CreateQuizz submitData={(data) => setQuizData(data)} />
+      <button
+        type="button"
+        className="m-4 p-2 rounded-xl bg-white cursor-pointer text-lg shadow-md hover:bg-black hover:text-white"
+        onClick={() => addNewBlockAndUpdateStep()}
+      >
+        <p>Nouvelle section</p>
+      </button>
     </div>
   );
 }
--- frontend/src/pages/data.js
+++ frontend/src/pages/data.js
@@ -1,5 +1,5 @@
 const data = [
-  { step: 1, content: "Voici du contenu pour la main data" },
+  { step: 1, text: "Voici du contenu pour la main data" },
   { title: "Step 1", objective: "lune", description: "This is step 1" },
 ];
 export default data;
