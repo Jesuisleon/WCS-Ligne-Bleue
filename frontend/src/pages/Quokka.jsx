be93e292d36033b4a5e134c3ffb8cc77aea4ea52
Rémy Bernardin <remy.bernardin.sen@gmail.com>
2023-01-16 14:13:19 +0100

    save

---
+++ frontend/src/pages/Quokka.jsx
@@ -0,0 +1,59 @@
+// import React, {useState} from "react";
+// const tutorials = [
+//     {
+//       id :1,
+//       tutorialName : "Conecter son téléphone aux wi-fi",
+//       hastag : ["téléphone"]
+//     },
+//     {
+//       id :2,
+//       tutorialName : "Utiliser son téléphone mobile",
+//       hastag : ["utiliser","téléphone","mobile"]
+//     },
+//     {
+//       id :3,
+//       tutorialName : "Eteindre son ordinateur",
+//       hastag : ["eteindre","ordinateur"]
+//     },
+//     {
+//       id :4,
+//       tutorialName : "Eteindre son téléphone",
+//       hastag : ["eteindre","téléphone"]
+//     },
+//     {
+//       id :5,
+//       tutorialName : "Utiliser son ordinateur",
+//       hastag : ["utiliser","ordinateur"]
+//     },
+//     {
+//       id :6,
+//       tutorialName : "Utiliser sa tablette",
+//       hastag : ["utiliser","tablette"]
+//     },
+//     {
+//       id :7,
+//       tutorialName : "Eteindre sa tablette",
+//       hastag : ["eteindre","tablette"]
+//     },
+//   ]
+//   console.log(tutorials[0].hastag[0])
+
+// const searchValue = "téléphone téléphone téléphone téléphone "
+
+
+// const splitPhrasing = (phrase, tableToConvert) =>{
+//     const phraseSplit = phrase.split(" ")
+//     console.log(phraseSplit)
+//     console.log(tableToConvert.filter((e)=> e.hastag.includes(phraseSplit.map((e) => e))))
+//     console.log(phraseSplit[0])
+//     console.log(phraseSplit.map((e) => e))
+
+//     return phraseSplit
+//   }
+
+//   splitPhrasing(searchValue, tutorials)
+
+//   tutorials.filter((e)=>e.hastag.includes(phraseSplit.map((a)=> a)))
+
+
+
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,5 +1,88 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Search() {
-  return <div>Search</div>;
+  const [searchValue, setSearchValue] = useState("");
+
+  const tutorials = [
+    {
+      id :1,
+      tutorialName : "Conecter son téléphone aux wi-fi",
+      hastag : ["téléphone","wi-fi"]
+    },
+    {
+      id :2,
+      tutorialName : "Utiliser son téléphone mobile",
+      hastag : ["utiliser","téléphone","mobile"]
+    },
+    {
+      id :3,
+      tutorialName : "Eteindre son ordinateur",
+      hastag : ["eteindre","ordinateur"]
+    },
+    {
+      id :4,
+      tutorialName : "Eteindre son téléphone",
+      hastag : ["eteindre","téléphone"]
+    },
+    {
+      id :5,
+      tutorialName : "Utiliser son ordinateur",
+      hastag : ["utiliser","ordinateur"]
+    },
+    {
+      id :6,
+      tutorialName : "Utiliser sa tablette",
+      hastag : ["utiliser","tablette"]
+    },
+    {
+      id :7,
+      tutorialName : "Eteindre sa tablette",
+      hastag : ["eteindre","tablette"]
+    },
+
+  ]
+
+ 
+
+  const email= "blabla"
+  return (
+    <div
+      className="
+    flex-grow
+    flex
+    flex-col
+    px-20
+    align-center
+    "
+    >
+      <form onSubmit={()=> console.log("blabla")}>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="flex justify-center mb-2 text-sm font-medium text-gray-900 dark:text-dark mt-2"
+          >
+            Veuillez renseigner ci-dessous votre recherche
+          </label>
+          <input
+            type="search"
+            id="email"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Exemple : Tutoriel pour apprendre à allumer sont téléphone "
+            required
+          />
+        </div>
+      </form>
+      {tutorials
+      .filter((e)=> e.hastag.includes(searchValue) )
+      .map((e)=>
+      <div className="mt-2">
+       <h1>{e.tutorialName}</h1> 
+       <h2>{e.hastag}</h2> 
+      </div>
+      )}
+
+    </div>
+    )
 }
