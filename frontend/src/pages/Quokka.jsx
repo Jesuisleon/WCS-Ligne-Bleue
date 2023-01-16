ec2a141222a0f791aab956016af2eaba926a2fa7
Rémy Bernardin <remy.bernardin.sen@gmail.com>
2023-01-16 16:29:06 +0100

    commit message

--- frontend/src/pages/Quokka.jsx
+++ frontend/src/pages/Quokka.jsx
@@ -1,59 +1,75 @@
-// import React, {useState} from "react";
-// const tutorials = [
-//     {
-//       id :1,
-//       tutorialName : "Conecter son téléphone aux wi-fi",
-//       hastag : ["téléphone"]
-//     },
-//     {
-//       id :2,
-//       tutorialName : "Utiliser son téléphone mobile",
-//       hastag : ["utiliser","téléphone","mobile"]
-//     },
-//     {
-//       id :3,
-//       tutorialName : "Eteindre son ordinateur",
-//       hastag : ["eteindre","ordinateur"]
-//     },
-//     {
-//       id :4,
-//       tutorialName : "Eteindre son téléphone",
-//       hastag : ["eteindre","téléphone"]
-//     },
-//     {
-//       id :5,
-//       tutorialName : "Utiliser son ordinateur",
-//       hastag : ["utiliser","ordinateur"]
-//     },
-//     {
-//       id :6,
-//       tutorialName : "Utiliser sa tablette",
-//       hastag : ["utiliser","tablette"]
-//     },
-//     {
-//       id :7,
-//       tutorialName : "Eteindre sa tablette",
-//       hastag : ["eteindre","tablette"]
-//     },
-//   ]
-//   console.log(tutorials[0].hastag[0])
+import React, {useState} from "react";
+const tutorials = [
+    {
+      id :1,
+      tutorialName : "Conecter son téléphone aux wi-fi",
+      hashtag: ["téléphone"]
+    },
+    {
+      id :2,
+      tutorialName : "Utiliser son téléphone mobile",
+      hashtag: ["utiliser","téléphone","mobile"]
+    },
+    {
+      id :3,
+      tutorialName : "Eteindre son ordinateur",
+      hashtag: ["eteindre","ordinateur"]
+    },
+    {
+      id :4,
+      tutorialName : "Eteindre son téléphone",
+      hashtag: ["eteindre","téléphone"]
+    },
+    {
+      id :5,
+      tutorialName : "Utiliser son ordinateur",
+      hashtag: ["utiliser","ordinateur"]
+    },
+    {
+      id :6,
+      tutorialName : "Utiliser sa tablette",
+      hashtag: ["utiliser","tablette"]
+    },
+    {
+      id :7,
+      tutorialName : "Eteindre sa tablette",
+      hashtag: ["eteindre","tablette"]
+    },
+  ]
 
-// const searchValue = "téléphone téléphone téléphone téléphone "
+  const phrases = ["téléphone", "tablette", "voiture" ]
 
 
-// const splitPhrasing = (phrase, tableToConvert) =>{
-//     const phraseSplit = phrase.split(" ")
-//     console.log(phraseSplit)
-//     console.log(tableToConvert.filter((e)=> e.hastag.includes(phraseSplit.map((e) => e))))
-//     console.log(phraseSplit[0])
-//     console.log(phraseSplit.map((e) => e))
+  console.log(tutorials.filter((e)=> e.hashtag.includes("téléphone") || e.hashtag.includes("blabla") ))
 
-//     return phraseSplit
-//   }
+  tutorials.filter((e)=>e.hashtag.includes())
 
-//   splitPhrasing(searchValue, tutorials)
 
-//   tutorials.filter((e)=>e.hastag.includes(phraseSplit.map((a)=> a)))
+
+
+
+
+
+
+
+  console.log(tutorials[0].hastag[0])
+
+const searchValue = "téléphone téléphone téléphone téléphone "
+
+
+const splitPhrasing = (phrase, tableToConvert) =>{
+    const phraseSplit = phrase.split(" ")
+    console.log(phraseSplit)
+    console.log(tableToConvert.filter((e)=> e.hastag.includes(phraseSplit.map((e) => e))))
+
+    return phraseSplit
+  }
+
+  splitPhrasing(searchValue, tutorials)
+
+  tutorials.filter((e)=>e.hastag.includes(phraseSplit.map((a)=> a)))
+
+
 
 
 
