64213250bac39dc59870b341932e4022ddea666d
Rémy Bernardin <remy.bernardin.sen@gmail.com>
2023-01-18 12:00:13 +0100

    SearchFilter la recherche est maintenant en temps réel

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchTutorial from "../services/searchFonction";
 
 export default function Search() {
@@ -43,11 +43,10 @@ export default function Search() {
     },
   ];
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  useEffect(() => {
     const searchResulteArray = SearchTutorial(searchValue, tutorials);
     setSearchResult(searchResulteArray);
-  };
+  }, [searchValue]);
 
   return (
     <div
@@ -59,25 +58,23 @@ export default function Search() {
     align-center
     "
     >
-      <form onSubmit={handleSubmit}>
-        <div className="mb-6">
-          <label
-            htmlFor="email"
-            className="flex justify-center mb-2 text-sm font-medium text-gray-900 dark:text-dark mt-2"
-          >
-            Veuillez renseigner ci-dessous votre recherche
-          </label>
-          <input
-            type="input"
-            id="search"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Exemple : Tutoriel pour apprendre à allumer sont téléphone "
-            required
-          />
-        </div>
-      </form>
+      <div className="mb-6">
+        <label
+          htmlFor="search"
+          className="flex justify-center mb-2 text-sm font-medium text-gray-900 dark:text-dark mt-2"
+        >
+          Veuillez renseigner ci-dessous votre recherche
+        </label>
+        <input
+          type="input"
+          id="search"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Exemple : Tutoriel pour apprendre à allumer sont téléphone "
+          required
+        />
+      </div>
       {searchResult &&
         searchResult.map((e) => (
           <div key={e.id} className="mt-2">
