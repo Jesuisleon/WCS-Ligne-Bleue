2f085443aa93c47642973700e2858aa13ba223d6
lucas fasilleau <lucasfaz@hotmail.fr>
2023-01-25 17:24:31 +0100

    new style

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import axios from "axios";
-import { BsFillArrowDownSquareFill } from "react-icons/bs";
+
 import { FaSearch } from "react-icons/fa";
 import { motion } from "framer-motion";
-import logoPoste from "../../public/image/logo_la_poste.png";
+
 import SearchTutorial from "../services/utils/searchFonction";
 
 export default function Search() {
   const [searchValue, setSearchValue] = useState("");
   const [searchResult, setSearchResult] = useState("");
   const [tutorialInfos, setTutorialInfos] = useState("");
-  const [isHidden, setIsHidden] = useState(false);
 
   useEffect(() => {
     axios.get(`/tutorials-search`).then((response) => {
@@ -24,44 +23,49 @@ export default function Search() {
     if (tutorialInfos) {
       const searchResulteArray = SearchTutorial(searchValue, tutorialInfos);
       setSearchResult(searchResulteArray);
-      setIsHidden(searchValue !== "");
-    } else {
-      setIsHidden(false);
     }
   }, [searchValue]);
 
   return (
     <div className="flex-grow flex flex-col px-60 py-5 align-center ">
-      {isHidden ? null : (
-        <div className="bg-gradient-to-b from-blue-700 to-blue-900 p-8 sm:p-4 flex flex-col justify-center items-center rounded-xl  shadow-lg  text-center">
-          <img
-            className="mx-auto h-12 w-auto mb-4"
-            src={logoPoste}
-            alt="Workflow"
-          />
-          <div className="text-2xl font-bold text-center flex align-center">
-            <FaSearch className="h-8 w-8 text-yellow-400 group-hover:text-[#1d4ed8] mr-2 mb-4 " />
-            <span className="text-white">
-              Veuillez renseigner ci-dessous votre recherche
-            </span>
-          </div>
-          <div className="text-sm font-medium text-white ">
-            <div className="text-xl font-bold text-left mb-auto">
-              Exemples :
-            </div>
-            <ul className="text-left  justify-center mt-2 text-base text-white sm:text-lg dark:text-white">
-              <li className="mb-2">
-                1- Tutoriel pour apprendre à allumer son téléphone
-              </li>
-              <li className="mb-2">2- Comment envoyer un mail</li>
-              <li>3- Comment charger son téléphone</li>
-            </ul>
-            <div className="d-grid place-items-center">
-              <BsFillArrowDownSquareFill className="h-8 w-8 text-yellow-400 group-hover:text-[#1d4ed8] mt-4 ml-auto mr-auto" />
-            </div>
-          </div>
+      {/* {isHidden ? null : ( */}
+      <div className="bg-gradient-to-b from-blue-700 to-blue-900 p-8 sm:p-4 flex flex-col justify-center items-center rounded-xl  shadow-lg  text-center">
+        <div className="text-2xl font-bold text-center flex align-center">
+          <FaSearch className="h-8 w-8 text-yellow-400 group-hover:text-[#1d4ed8] mr-2 mb-4 " />
+          <span className="text-white">
+            Veuillez renseigner ci-dessous votre recherche
+          </span>
         </div>
-      )}
+        <div className="text-sm font-medium text-white ">
+          <div className="text-xl font-bold text-left mb-auto">Exemples :</div>
+          <ul className="text-left  justify-center mt-2 text-base text-white sm:text-lg dark:text-white">
+            <li className="mb-2">
+              1- Tutoriel pour apprendre à allumer son téléphone
+            </li>
+            <li className="mb-2">2- Comment envoyer un mail</li>
+            <li>3- Comment charger son téléphone</li>
+          </ul>
+        </div>
+      </div>
+      {/* )} */}
+
+      <div className="mb-6">
+        <label
+          htmlFor="search"
+          className="flex justify-center mb-2 text-sm font-medium text-gray-900 dark:text-dark mt-2"
+        >
+          {" "}
+        </label>
+        <input
+          type="input"
+          id="search"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="bg-gray-700  border-indigo-800 text-gray-900 text-sm rounded-lg focus:ring-blue-500 border-4  focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500  dark:focus:border-indigo-700"
+          placeholder="Exemple : Tutoriel pour apprendre à allumer sont téléphone "
+          required
+        />
+      </div>
 
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {searchResult &&
@@ -86,24 +90,6 @@ export default function Search() {
               </motion.div>
             </Link>
           ))}
-      </div>
-
-      <div className="mb-6">
-        <label
-          htmlFor="search"
-          className="flex justify-center mb-2 text-sm font-medium text-gray-900 dark:text-dark mt-2"
-        >
-          {" "}
-        </label>
-        <input
-          type="input"
-          id="search"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          className="bg-gray-700  border-indigo-800 text-gray-900 text-sm rounded-lg focus:ring-blue-500 border-4  focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500  dark:focus:border-indigo-700"
-          placeholder="Exemple : Tutoriel pour apprendre à allumer sont téléphone "
-          required
-        />
       </div>
     </div>
   );
