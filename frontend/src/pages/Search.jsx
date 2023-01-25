027bb3875ba93c57efa5939372203118d2d059ad
lucas fasilleau <lucasfaz@hotmail.fr>
2023-01-25 15:41:36 +0100

    style searchPage

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import axios from "axios";
-import { BsFillArrowUpCircleFill } from "react-icons/bs";
+import { BsFillArrowDownSquareFill } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
 import logoPoste from "../../public/image/logo_la_poste.png";
 import SearchTutorial from "../services/utils/searchFonction";
 
@@ -29,16 +30,55 @@ export default function Search() {
   }, [searchValue]);
 
   return (
-    <div
-      className="
-    flex-grow
-    flex
-    flex-col
-    px-20
-    align-center
-    
-    "
-    >
+    <div className="flex-grow flex flex-col px-60 py-5 align-center ">
+      {isHidden ? null : (
+        <div className="bg-gradient-to-b from-blue-700 to-blue-900 p-8 sm:p-4 flex flex-col justify-center items-center rounded-xl  shadow-lg  text-center">
+          <img
+            className="mx-auto h-12 w-auto mb-4"
+            src={logoPoste}
+            alt="Workflow"
+          />
+          <div className="text-2xl font-bold text-center flex align-center">
+            <FaSearch className="h-8 w-8 text-yellow-400 group-hover:text-[#1d4ed8] mr-2 mb-4 " />
+            <span className="text-white">
+              Veuillez renseigner ci-dessous votre recherche
+            </span>
+          </div>
+          <div className="text-sm font-medium text-white ">
+            <div className="text-xl font-bold text-left mb-auto">
+              Exemples :
+            </div>
+            <ul className="text-left  justify-center mt-2 text-base text-white sm:text-lg dark:text-white">
+              <li className="mb-2">
+                1- Tutoriel pour apprendre à allumer son téléphone
+              </li>
+              <li className="mb-2">2- Comment envoyer un mail</li>
+              <li>3- Comment charger son téléphone</li>
+            </ul>
+            <div className="d-grid place-items-center">
+              <BsFillArrowDownSquareFill className="h-8 w-8 text-yellow-400 group-hover:text-[#1d4ed8] mt-4 ml-auto mr-auto" />
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        {searchResult &&
+          searchResult.map((e) => (
+            <Link key={e.id} to={`/tutorial/${e.id}`}>
+              <div
+                key={e.id}
+                className="bg-gradient-to-b from-yellow-300 to-yellow-500 p-8 rounded-xl shadow-lg transition duration-200 ease-in-out transform hover:scale-110 text-center "
+              >
+                <h1 className="text-lg font-medium overflow-hidden">
+                  {e.title}
+                </h1>
+                <h2 className="text-sm font-medium text-gray-700">{e.theme}</h2>
+              </div>
+            </Link>
+          ))}
+      </div>
+
       <div className="mb-6">
         <label
           htmlFor="search"
@@ -51,49 +91,11 @@ export default function Search() {
           id="search"
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
-          className="bg-gray-700 border border-indigo-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-indigo-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-700  border-indigo-800 text-gray-900 text-sm rounded-lg focus:ring-blue-500 border-4  focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500  dark:focus:border-indigo-700"
           placeholder="Exemple : Tutoriel pour apprendre à allumer sont téléphone "
           required
         />
       </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {searchResult &&
-          searchResult.map((e) => (
-            <Link key={e.id} to={`/tutorial/${e.id}`}>
-              <div
-                key={e.id}
-                className="bg-gradient-to-b from-yellow-300 to-yellow-500 p-8 rounded-xl shadow-lg transition duration-200 ease-in-out transform hover:scale-110 text-center "
-              >
-                <h1 className="text-xl font-medium">{e.title}</h1>
-                <h2 className="text-sm font-medium text-gray-700">{e.theme}</h2>
-              </div>
-            </Link>
-          ))}
-      </div>
-      {isHidden ? null : (
-        <div className="bg-gradient-to-b from-blue-700 to-blue-900  h-50% w-50% p-8 sm:p-10 flex flex-col justify-center items-center rounded-xl transition duration-200 ease-in-out transform hover:scale-110 shadow-lg  text-center">
-          <img
-            className="mx-auto h-12 w-auto mb-4"
-            src={logoPoste}
-            alt="Workflow"
-          />
-          <div className="text-xl font-bold text-center flex align-center">
-            <BsFillArrowUpCircleFill className="h-8 w-8 text-yellow-400 group-hover:text-[#1d4ed8] mr-2" />
-            <span className="text-white">
-              Veuillez renseigner ci-dessus votre recherche
-            </span>
-            <BsFillArrowUpCircleFill className="h-8 w-8 text-yellow-400 group-hover:text-[#1d4ed8] ml-2" />
-          </div>
-          <div className="text-sm font-medium text-white mb-2">
-            <div className="text-xl font-bold text-center">Exemples :</div>
-            <ul className="text-left justify-center mb-5 text-base text-white sm:text-lg dark:text-white">
-              <li>1- Tutoriel pour apprendre à allumer son téléphone</li>
-              <li>2- Comment envoyer un mail</li>
-              <li>3- Comment charger son téléphone</li>
-            </ul>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
