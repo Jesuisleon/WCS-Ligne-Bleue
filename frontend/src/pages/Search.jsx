7d311989dd13b2f1acf2730449713e356786601e
Jesuisleon <frederic-versavel_student2022@wilder.school>
2023-02-08 10:08:58 +0100

    FIX: poussin hors jeux

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -16,7 +16,6 @@ export default function Search() {
   const [data, setData] = useState();
 
   const [tutorial, setTutorial] = useState([]);
-  console.log("🚀 ~ Search ~ tutorial", tutorial)
 
   const [searchValue, setSearchValue] = useState("");
 
